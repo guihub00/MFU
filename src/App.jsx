42e71204c6bd48a4f2390247b8b1f686566a0c52
Wellington S. Ferreira Junior <wellingtonsoaresfe.jr@gmail.com>
2024-08-13 22:58:31 +0000

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { Header } from "./components/header.style"
+import { Main } from "./components/main.style"
 
 function App() {
   
   return (
-   <Header>
-      <h1>ola</h1>
-   </Header>
+    <>
+      <Header>
+        <h1>ola</h1>
+      </Header>
+
+      <Main>
+        
+      </Main>
+    </>
+   
   )
   
 }
